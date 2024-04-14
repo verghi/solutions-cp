c697e3f571b53be6dbb1d08c4bdbd6e7bf718686
verghi <137410148+verghi@users.noreply.github.com>
2024-04-14 23:11:24 +0200

    unique paths dynamic programming solution

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int uniquePaths(int m, int n) {
+    int t[m][n];
+    for (int i=0;i<m;i++)
+    {
+        for (int j=0;j<n;j++)
+        {
+            t[i][j]=0;
+        }
+    }
+    for (int j=0;j<n;j++)
+    {
+        t[0][j]=1;
+    }
+    for (int i=1;i<m;i++)
+    {
+        t[i][0]=1;
+    }
+    for (int i=1;i<m;i++)
+    {
+        for (int j=1;j<n;j++)
+        {
+            t[i][j]=t[i-1][j] + t[i][j-1];
+        }
+    }
+    return t[m-1][n-1];
+}
+
+int main()
+{
+    int res=uniquePaths(1,1);
+    cout << res;
+    return 0;
+}
